8f6ee48ea05d6b985ea9014141a97724ba903fb4
ravik0205 <ravik.data02@gmail.com>
2025-06-18 05:52:40 -0500

    Fix destination folder to todo_app_project in CodeDeploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Navigate to app directory
-cd /home/ec2-user/flask-app
+cd /home/ec2-user/todo_app_project
 
-# Set up virtual environment if not exists
+# Create and activate virtual environment
 if [ ! -d ".venv" ]; then
   python3 -m venv .venv
 fi
 
-# Activate virtual environment
 source .venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Kill anything running on port 8000 (or 5000 if you're using that)
+# Kill any process running on port 8000
 fuser -k 8000/tcp || true
 
-# Start the app in the background
+# Start the app
 nohup python run.py > app.log 2>&1 &
+
